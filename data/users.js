d5d638dcca3b1dfe36195c4819ff4e576d6a5196
naman-palindrome <namanahuja80@gmail.com>
2022-01-05 21:09:06 +0530

    first commit

---
+++ data/users.js
@@ -0,0 +1,21 @@
+import bcrypt from "bcryptjs";
+const users = [
+  {
+    name: "Admin user",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: " user1",
+    email: "user1@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "user2",
+    email: "user2@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
